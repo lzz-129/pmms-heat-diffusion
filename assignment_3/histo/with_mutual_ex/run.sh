c4c631db1bf9df1bf3c030df608c05408e74c6bd
yaojia1 <yplus1cn@gmail.com>
2023-03-14 23:53:12 +0100

    experiments

--- assignment_3/histo/with_mutual_ex/run.sh
+++ assignment_3/histo/with_mutual_ex/run.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-IMG=$HOME/pmms-heat-diffusion/images/pat1_"$2"x"$3".pgm
+IMG=$HOME/pmms-heat-diffusion/images/gradient_"$2"x"$3".pgm
 ls $IMG
-for i in {1..32}
+for i in histo_atomic histo_mutex histo_semaphores histo_sw_transactional
 do
-   "$1" -i $IMG -m "$2" -n "$3" -p "$i" | grep Histo |awk '{print($3)}'
+   echo $i
+   for j in {1..16}
+   do
+      ./$i/$i -i $IMG -m "$2" -n "$3" -p $j | grep Histo |awk '{print($3)}'
+   done
 done
 
