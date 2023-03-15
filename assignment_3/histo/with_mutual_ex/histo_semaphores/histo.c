3b12f07bcc461fe42470debf4a4934e3786da262
yaojia1 <yplus1cn@gmail.com>
2023-03-15 20:38:39 +0100

    merge

--- assignment_3/histo/with_mutual_ex/histo_semaphores/histo.c
+++ assignment_3/histo/with_mutual_ex/histo_semaphores/histo.c
@@ -14,7 +14,8 @@ struct param{
     int start;
     int len;
 };
-sem_t sems[256];
+
+sem_t sem;
 
 void die(const char *msg){
     if (errno != 0) 
@@ -89,16 +90,15 @@ void* histogram(void* p_data){
     struct param* myparam = (struct param*) p_data;
     int * tmp_histo = (int *) calloc(256, sizeof(int));
     int res = 0;
-    sem_t sem;
     for(int i = myparam->start; i < myparam->start+myparam->len; i++){
         res = myparam->image[i];
         tmp_histo[res] += 1;
     }
     for(int i = 0; i < 256; i++){
         if(tmp_histo[i]==0) continue;
-        sem = sems[res];
         sem_wait(&sem); //sem --
         myparam->histo[i] += tmp_histo[i];
+        usleep(20);
         sem_post(&sem); //sem++
     }
     free(tmp_histo);
@@ -169,10 +169,9 @@ int main(int argc, char *argv[]){
     int seg = (int) num_cols*num_rows/num_threads;
     struct param* my_param;
     pthread_t threads[num_threads];
-    for(int i = 0; i<256; i++){
-        sem_init(&sems[i], 0, 1);
-        //sem_open("sem[i]", O_CREAT|O_EXCL, S_IRWXU, 0);
-    }
+
+    sem_init(&sem, 0, 1); //for gcc
+    //sem_open("sem", O_CREAT|O_EXCL, S_IRWXU, 0); //for mac
     for(int t = 0; t<num_threads; t++){
         my_param = (struct param*)malloc(sizeof(struct param));
         my_param->histo = histo;
@@ -189,10 +188,8 @@ int main(int argc, char *argv[]){
     for(int t = 0; t<num_threads; t++){
         pthread_join(threads[t], NULL);
     }
-    for(int i = 0; i<256; i++){
-        //sem_unlink("sems[i]"); //for mac
-        sem_destroy(&sems[i]); //for gcc
-    }
+    sem_destroy(&sem); //for gcc
+    //sem_unlink("sem");
     //histogram(histo, image);
     /* Do your thing here */
 
